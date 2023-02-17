f759850abdf4b39f1051841c838d7a2fd4c0da55
Ian <chemwenokibet11@gmail.com>
2023-02-18 00:33:53 +0300

    created a method returns an array of authors who have written more than 2 articles

--- magazine.rb
+++ magazine.rb
@@ -15,4 +15,14 @@ class Magazine
     def article_titles
         Article.all.select {|article| article.magazine == self }.map(&:title)
     end    
+
+    def contributing_authors
+        author_counts = Hash.new(0)
+        Article.all.select{|article| article.magazine == self }.each do |article|
+            author_counts [article.author] += 1
+        end
+        author_counts.select {|author, count| count > 2 }.keys
+    end
+    
+    
 end    
