783f64ed14871c896d35dd599f4fb28e8cd1fc0d
Ian <chemwenokibet11@gmail.com>
2023-02-17 23:02:15 +0300

    created the initialize function with name and category as parameters

--- magazine.rb
+++ magazine.rb
@@ -1,4 +1,10 @@
 class Magazine 
     attr_reader :name, :category
     @@all = []
+
+    def initialize(name, category)
+        @name = name
+        @category = category
+        @@name << self
+    end    
 end    
