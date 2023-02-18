36135ed113a86f3f2c26da2e2e119650bfe69f82
Ian <chemwenokibet11@gmail.com>
2023-02-18 11:31:55 +0300

    added comments to the functions

--- author.rb
+++ author.rb
@@ -1,26 +1,30 @@
 class Author
-    attr_reader :name
+    attr_reader :name #attribute to set authors name
 
     def initialize(name)
         @name = name
-        @articles = []
+        @articles = [] #empty array to hold articles written by author
     end
     
+    #returns frozen duplicate to prevent modification
     def articles
-        @articles.dup.freeze
+        @articles.dup.freeze 
     end
     
+    #return list of unique magazines that author has wrtten
     def magazines
-        @articles.map(&:magazine).uniq
+        @articles.map(&:magazine).uniq  
     end
     
-    def add_article(magazine, title)
+    #takes mag and article arguments to create new article instances and add it to @articles array
+    def add_article(magazine, title)  
         article = Article.new(self, magazine, title)
         @articles << article
         article
     end
     
-    def topic_areas
+    #returns a list of unique categories of magz that author has written
+    def topic_areas 
         magazines.map(&:category).uniq
     end    
 
--- magazine.rb
+++ magazine.rb
@@ -8,22 +8,26 @@ class Magazine
         @@name << self
     end    
 
-    def self.all
+    #returns a frozen duplicate of an array containing all the Magazine instances.
+    def self.all  
         @@all.dup.freeze
     end    
 
-    def article_titles
+    #returns a list of article titles published by mag instances
+    def article_titles #returns a list of article titles published by mag instances
         Article.all.select {|article| article.magazine == self }.map(&:title)
     end    
 
-    def contributing_authors
-        author_counts = Hash.new(0)
+    #returns a list of authors who have contributed more than two articles to the magazine instance
+    def contributing_authors 
+        author_counts = Hash.new(0)  
         Article.all.select{|article| article.magazine == self }.each do |article|
             author_counts [article.author] += 1
         end
         author_counts.select {|author, count| count > 2 }.keys
     end
     
+    #takes a name argument and returns the magazine instance with that name.
     def self.find_by_name(name)
         @@all.find {|magazine| magazine.name == name }
     end    
